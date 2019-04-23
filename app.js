77d64d6af7f26b9c72418a9c56619d6ae45743a6
Justine Young <justiney95@gmail.com>
2019-04-23 00:45:24 -0700

    checkpoint-5-DOM-pt1: completed

---
+++ app.js
@@ -0,0 +1,16 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    //get the text
+    let title = newToDoText.value;
+  });
+}
+
+window.onload = function() {
+  onReady();
+};
